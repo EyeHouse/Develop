a8e4097eb4339f481cc277baee7faef5d865bd86
rjm548 <rjm548@ELCLASS23.its.york.ac.uk>
2015-03-04 18:00:20 +0000

    Changed imageHandler to use Java FX - image position is relative to the main window as in the XML.
    Signed-off-by: rjm548 <rjm548@ELCLASS23.its.york.ac.uk>

--- EyeView/src/ImageHandler.java
+++ EyeView/src/ImageHandler.java
@@ -1,70 +1,51 @@
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.awt.image.BufferedImage;
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Graphics;
-import java.io.File;
-import java.io.IOException;
-import java.awt.Window;
+import javafx.scene.Group;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
-import javax.imageio.ImageIO;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+public class ImageHandler {
 
-public class ImageHandler extends JPanel {
-	
-	//String sourceFile;
-	float xStart;
-	float yStart;
+	String sourcefile;
+	float xstart;
+	float ystart;
 	float scale;
 	float duration;
-	float startTime;
-	BufferedImage image;
-	
-	public ImageHandler() {
+	float starttime;
+	Group group;
+	float xResolution;
+	float yResolution;
+
+	public ImageHandler(Group group, float xResolution, float yResolution) {
 		
-	    setBackground(Color.white);
-	    
-		try {                
-	          image = ImageIO.read(new File("crazy.png"));
-	       } catch (IOException ex) {
-	    	   System.out.println("there has been an error");
-	       } 
-		
-		repaint();
-		makeFrame();
+		this.group = group;
+		this.xResolution = xResolution;
+		this.yResolution = yResolution;
 	}
 
-	public void paintComponent(Graphics g){
-		int x = 100;
-		int y = 100; 	
-		g.drawImage(image, x, y, null); // see javadoc for more info on the parameters  
-	}
+	public void ImageElement(String sourcefile, float xstart, float ystart,
+			float scale, float duration, float starttime) {
+
+		float xPosition;
+		float yPosition;
+		float width;
+		float height;
+		
+		this.sourcefile = sourcefile;
+		this.xstart = xstart;
+		this.ystart = ystart;
+		this.scale = scale;
+		this.duration = duration;
+		this.starttime = starttime;
+
+		xPosition = xResolution * xstart;
+		yPosition = yResolution * ystart;
+		
+		Image house = new Image(sourcefile);
+		ImageView houseImage = new ImageView(house);
 	
-	 public void makeFrame() {
-	        
-        // Instantiate a window frame using Swing class JFrame
-        JFrame frame = new JFrame("ShapeManipulation");
+		houseImage.setX(xPosition);
+		houseImage.setY(yPosition);
+				
+		group.getChildren().add(houseImage);
+	}
 
-        // When the window is closed terminate the application
-        frame.addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e) {
-                System.exit(0);
-            }
-        });
-
-        // Set initial size of window and prevent resizing
-        frame.setSize(1000, 700);
-        frame.setResizable(false);
-        
-        // Add the current object to the centre of the frame and make visible
-        frame.getContentPane().add(this, BorderLayout.CENTER);
-        frame.setBackground(Color.WHITE);
-        frame.setVisible(true);
-	}   	 
-	 
-	public static void main(String[] properties) {
-		new ImageHandler();
-	}	
 }
