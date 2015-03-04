c6c95a3ba2afc57c7970c913ff10b4149e1d59bc
aph513 <aph513@ELCLASS09.its.york.ac.uk>
2015-03-04 13:02:04 +0000

    Added combobox for birthday which is populated with the user information through string manipulation.
    Apply changes button added and layout set up but functionality not yet
    implemented.
    
    Signed-off-by: aph513 <aph513@ELCLASS09.its.york.ac.uk>

--- EyeView/src/Profile.java
+++ EyeView/src/Profile.java
@@ -7,13 +7,20 @@
  */
 
 import javafx.application.Application;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
+import javafx.geometry.VPos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.RowConstraints;
 
 public class Profile extends Application{
 
@@ -32,8 +39,14 @@ public class Profile extends Application{
 
 	public static final int xResolution = 800;
 	public static final int yResolution = 600;
+	public static final int gridCellWidth = 100;
+	public static final int gridCellHeight = 30;
 	public static final double xStart = 0.25;
 	public static final double yStart = 0.166;
+	
+	ComboBox<Integer> comboDoBDay = new ComboBox<Integer>();
+    ComboBox<Integer> comboDoBMonth = new ComboBox<Integer>();
+    ComboBox<Integer> comboDoBYear = new ComboBox<Integer>();
 	
 	ProfileItem user = new ProfileItem("John","Smith","JSmith1","j.smith@gmail.com","20/01/1993","Male");
 	
@@ -47,10 +60,13 @@ public class Profile extends Application{
 		
 		Group screenGroup = new Group();
 		GridPane grid = new GridPane();
+		HBox hBoxDoB = new HBox(5);
 		
-		grid.setHgap(100);
-        grid.setVgap(30);
+		grid.setHgap(gridCellWidth);
+        grid.setVgap(gridCellHeight);
         grid.setPadding(new Insets(yResolution*yStart,xResolution*xStart,100,200));
+        
+        
         
         Label labelFName = new Label("First Name");
         Label labelLName = new Label("Last Name");
@@ -63,19 +79,47 @@ public class Profile extends Application{
         TextField fieldLName = new TextField(user.lName);
         TextField fieldUsername = new TextField(user.username);
         TextField fieldEmail = new TextField(user.email);
-        TextField fieldDoB = new TextField(user.doB);
         TextField fieldGender = new TextField(user.gender);
         
+        Button buttonApply = new Button("Apply Changes");
+        //buttonApply.addEventHandler(MouseEvent.MOUSE_CLICKED, );
+        
+        SetupDoB();
+        
+        hBoxDoB.getChildren().addAll(comboDoBDay,comboDoBMonth,comboDoBYear);
+        
         grid.addColumn(0, labelFName, labelLName, labelUsername, labelEmail, labelDoB, labelGender);
-        grid.addColumn(1, fieldFName, fieldLName, fieldUsername, fieldEmail, fieldDoB, fieldGender);
+        grid.addColumn(1, fieldFName, fieldLName, fieldUsername, fieldEmail, hBoxDoB, fieldGender);
+        
+        grid.add(buttonApply, 0, 6);
+        GridPane.setConstraints(buttonApply,0,6,2,1,HPos.CENTER,VPos.CENTER);
         
         screenGroup.getChildren().add(grid);
 		primaryStage.setScene(new Scene(screenGroup, xResolution, yResolution));
 		primaryStage.show();
 	}
 	
+	public void SetupDoB(){
+		
+		for(int i = 1; i < 32; i++){
+        	comboDoBDay.getItems().add(i);
+        }
+        for(int i = 1; i < 13; i++){
+        	comboDoBMonth.getItems().add(i);
+        }
+        for(int i = 2015; i > 1919; i--){
+        	comboDoBYear.getItems().add(i);
+        }
+        
+    	int doBDay = Integer.valueOf(user.doB.substring(0,2), 10).intValue();
+    	int doBMonth = Integer.valueOf(user.doB.substring(3,5), 10).intValue();
+    	int doBYear = Integer.valueOf(user.doB.substring(6,10), 10).intValue();
+    	comboDoBDay.getSelectionModel().select(doBDay - 1);
+    	comboDoBMonth.getSelectionModel().select(doBMonth-1);
+    	comboDoBYear.getSelectionModel().select(2015 - doBYear);
+	}
+	
 	public static void main(String[] args) {
 		Application.launch(args);
 	}
-
 }
