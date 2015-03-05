ea1a4e33acd5dd9105ca6dcc2517183bd3c2804f
aph513 <aph513@ELCLASS22.its.york.ac.uk>
2015-03-05 21:45:36 +0000

    Implemented the instantiation of a graphics canvas from the GraphicHandler with shapes defined in the XML.
    Signed-off-by: aph513 <aph513@ELCLASS22.its.york.ac.uk>

--- EyeView/src/presenter/Window.java
+++ EyeView/src/presenter/Window.java
@@ -6,6 +6,7 @@ import parser.Graphic;
 import parser.Slideshow;
 import parser.XMLParser;
 import parser.Slide;
+import presenter.GraphicHandler.GraphicElement;
 //import presenter.GraphicHandler.GraphicElement;
 import javafx.application.Application;
 import javafx.scene.Group;
@@ -18,19 +19,23 @@ public class Window extends Application {
 	public enum SlideName {
 		LOGGEDOUT, HOME, LOGIN, REGISTER
 	}
+	
+	private static final double xResolution = 400;
+	private static final double yResolution = 400;
+	private static final int graphicSlide = 1;
+	
 	private Group root;
-	private static final double xResolution = 960;
-	private static final double yResolution = 720;
 	private Slideshow slideshow;
 	private List<Slide> slideList;
 	private Slide currentSlide;
-	private int slideIndex = 1;
+	private int slideIndex = graphicSlide;
 
 	public void init(Stage primaryStage) {
 		
 		/* Runs the XML parser */
 		XMLParser parser = new XMLParser();
 		slideshow = parser.loadSlideshow("EyeView.xml");
+		parser.printLists();
 		slideList = slideshow.getSlides();
 		
 		/* Initialises primary stage */
@@ -43,9 +48,6 @@ public class Window extends Application {
         root = new Group();
         loadSlide(slideIndex);
         
-        /*GraphicHandler gh = new GraphicHandler(root, xResolution, yResolution);
-        GraphicElement graphic = gh.new GraphicElement("oval", 0.25f, 0.25f, 0.75f, 0.75f, true, 0, "#FFFF0000", "#00000000");*/
-        
 	    primaryStage.setScene(new Scene(root));
               
 	}
@@ -54,25 +56,35 @@ public class Window extends Application {
 		
 		root.getChildren().clear();
 		currentSlide = slideList.get(id);
+		switch(id){
+		case graphicSlide:
+			LoadGraphics();
+			break;
+		default:
+			break;
+		}
 		
-		//GraphicHandler gh = new GraphicHandler(root, xResolution, yResolution);
-		/*List<Graphic> graphicList = slideList.get(id).getGraphicList();
+		//ImageHandler ih = new ImageHandler();
+		/* something else here */
+		//slide.getChildren().add(img);
+	}
+	
+	public void LoadGraphics(){
+		GraphicHandler gh = new GraphicHandler(root, xResolution, yResolution);
+		List<Graphic> graphicList = slideList.get(graphicSlide).getGraphicList();
+
 		for (Graphic currentGraphic : graphicList) {
 			GraphicElement graphic = gh.new GraphicElement(currentGraphic.getType(),
 					currentGraphic.getXstart(),
 					currentGraphic.getYstart(),
 					currentGraphic.getXend(),
 					currentGraphic.getYend(),
-					currentGraphic.isSolid(),
 					currentGraphic.getDuration(),
+					currentGraphic.isSolid(),
 					currentGraphic.getGraphicColor(),
-					currentGraphic.getGraphicColor());
-		}*/
-		
-		
-		//ImageHandler ih = new ImageHandler();
-		/* something else here */
-		//slide.getChildren().add(img);
+					currentGraphic.getShadingColor());
+			gh.AddShapeToCanvas(graphic);
+		}
 	}
 	
 	@Override
