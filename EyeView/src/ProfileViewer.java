b80a6e2c9e403261e55d52e1c3e34917ff3bbbeb
aph513 <aph513@ELCLASS22.its.york.ac.uk>
2015-03-05 17:27:07 +0000

    Added rating based stars and font changes
    Signed-off-by: aph513 <aph513@ELCLASS22.its.york.ac.uk>

--- EyeView/src/ProfileViewer.java
+++ EyeView/src/ProfileViewer.java
@@ -7,25 +7,20 @@
  */
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.HPos ;
 import javafx.geometry.Insets;
-import javafx.geometry.VPos;
-import javafx.scene.Cursor;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Polyline;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
 
 public class ProfileViewer extends Application{
 
@@ -58,9 +53,13 @@ public class ProfileViewer extends Application{
 	public static final int gridCellHeight = 30;
 	public static final double xStart = 0.5;
 	public static final double yStart = 0.166;
+	public static final int rating = 1;
 
-    Button buttonMessage = new Button("Message");
-    Polygon star = new Polygon(0,0,50,0,50,50,0,50);
+    public Button buttonMessage = new Button("Message");
+    public Polygon[] star = new Polygon[5];
+    public Polyline[] starOutline = new Polyline[5];
+    
+    public Label labelName, labelEmail, labelDoB;
 	
 	User user = new User("John","Smith","j.smith@gmail.com","JSmith1", false, "password","20/01/1993",false);
 	
@@ -75,10 +74,40 @@ public class ProfileViewer extends Application{
 		Group screenGroup = new Group();
 		GridPane grid = new GridPane();
 		VBox vBoxUserText = new VBox(10);
-		HBox hBoxNameMessage = new HBox(gridCellWidth);
+		HBox hBoxStars = new HBox(5);
 		
+		SetupStars();
+		SetupLabels();
+		
+		grid.setHgap(gridCellWidth);
+        grid.setVgap(gridCellHeight);
+        grid.setPadding(new Insets(yResolution*yStart,xResolution*xStart,100,200));
+        
+        Rectangle profilePicture = new Rectangle(120,120, Color.GRAY);
+        
+        for(int i = 0 ; i < 5 ; i++){
+        	if(i<rating)hBoxStars.getChildren().add(star[i]);
+        	else hBoxStars.getChildren().add(starOutline[i]);
+        }
+
+        vBoxUserText.getChildren().addAll(labelName,labelEmail,labelDoB);
+        
+        grid.addRow(0, profilePicture, vBoxUserText);
+        grid.addRow(1, hBoxStars);
+        
+        screenGroup.getChildren().add(grid);
+        
+		primaryStage.setScene(new Scene(screenGroup, xResolution, yResolution));
+		primaryStage.show();
+	}
+	
+	public void GetProfile(){
+		//GetCurrentUser();
+	}
+	
+	public void SetupStars(){
 		int nbSides = 2*5;
-		int r = 100;
+		int r = 10;
         int[] xpoints = new int[nbSides];
         int[] ypoints = new int[nbSides];
         double angle = Math.PI/2;
@@ -93,44 +122,31 @@ public class ProfileViewer extends Application{
             }
             angle += Math.PI/5;
         }
-        //star = new Polygon(xpoints, ypoints, nbSides);
+        for(int i = 0; i < 5 ; i++){
+	        star[i] = new Polygon(	xpoints[0], ypoints[0],xpoints[1], ypoints[1],
+			       					xpoints[2], ypoints[2],xpoints[3], ypoints[3],
+			       					xpoints[4], ypoints[4],xpoints[5], ypoints[5],
+			       					xpoints[6], ypoints[6],xpoints[7], ypoints[7],
+			       					xpoints[8], ypoints[8],xpoints[9], ypoints[9]);
+	        starOutline[i] = new Polyline(	xpoints[0], ypoints[0],xpoints[1], ypoints[1],
+											xpoints[2], ypoints[2],xpoints[3], ypoints[3],
+											xpoints[4], ypoints[4],xpoints[5], ypoints[5],
+											xpoints[6], ypoints[6],xpoints[7], ypoints[7],
+											xpoints[8], ypoints[8],xpoints[9], ypoints[9],
+											xpoints[0], ypoints[0]);
+        }
+	}
+	
+	public void SetupLabels(){
+		Font fontTitle = new Font("Cambria", 24);
+		Font fontMain = new Font("Cambria", 18);
 		
-		grid.setHgap(gridCellWidth);
-        grid.setVgap(gridCellHeight);
-        grid.setPadding(new Insets(yResolution*yStart,xResolution*xStart,100,200));
-        
-        Rectangle profilePicture = new Rectangle(75,75, Color.GRAY);
-        
-        Label labelFName = new Label(user.fName);
-        Label labelEmail = new Label("Email: " + user.email);
-        Label labelDoB = new Label("Date of Birth: " + user.doB);
-        //double c = hBoxNameMessage.g
-        //Label labelC = new Label(String.valueOf(c));
-        
-        hBoxNameMessage.getChildren().addAll(labelFName, buttonMessage);
-        vBoxUserText.getChildren().addAll(hBoxNameMessage,labelEmail,labelDoB);
-        
-        buttonMessage.setOnAction(new EventHandler<ActionEvent>(){
-        	@Override public void handle(ActionEvent event){
-        		sendMessage();
-            }
-        });
-        
-        grid.addRow(0, profilePicture, vBoxUserText);
-        grid.addRow(1, star);
-        
-        screenGroup.getChildren().add(grid);
-        
-		primaryStage.setScene(new Scene(screenGroup, xResolution, yResolution));
-		primaryStage.show();
-	}
-	
-	public void GetProfile(){
-		//GetCurrentUser();
-	}
-	
-	private void sendMessage(){
-		buttonMessage.setVisible(false);
+		labelName = new Label(user.fName + "\t(" + user.username + ")");
+		labelName.setFont(fontTitle);
+        labelEmail = new Label("Email: " + user.email);
+        labelEmail.setFont(fontMain);
+        labelDoB = new Label("Date of Birth: " + user.doB);
+        labelDoB.setFont(fontMain);
 	}
 	
 	public static void main(String[] args) {
