ded54e2617a39ecddeba1dfb7233318a852acb9e
pjb493 <peter.bloxidge@virgin.net>
2015-03-05 18:20:40 +0000

    First commit of Slide branch Window opens, uses loadSlide() to load chosen slide by ID
    Signed-off-by: pjb493 <peter.bloxidge@virgin.net>

--- EyeView/src/presenter/Window.java
+++ EyeView/src/presenter/Window.java
@@ -2,39 +2,77 @@ package presenter;
 
 import java.util.List;
 
+import parser.Graphic;
 import parser.Slideshow;
 import parser.XMLParser;
 import parser.Slide;
+//import presenter.GraphicHandler.GraphicElement;
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Window extends Application {
 	
+	public enum SlideName {
+		LOGGEDOUT, HOME, LOGIN, REGISTER
+	}
+	private Group root;
+	private static final double xResolution = 960;
+	private static final double yResolution = 720;
 	private Slideshow slideshow;
 	private List<Slide> slideList;
-	private SlideContent sc;
+	private Slide currentSlide;
+	private int slideIndex = 1;
 
 	public void init(Stage primaryStage) {
 		
+		/* Runs the XML parser */
 		XMLParser parser = new XMLParser();
-		slideshow = parser.loadSlideshow("Example PWS XML.xml");
+		slideshow = parser.loadSlideshow("EyeView.xml");
 		slideList = slideshow.getSlides();
-		/* Initialises primary stage with an empty group */
 		
+		/* Initialises primary stage */
 		primaryStage.setTitle(slideshow.getTitle());
-        primaryStage.setWidth(960);
-        primaryStage.setHeight(720);
+		primaryStage.getIcons().add(new Image("file:./resources/icons/xxxhdpi.png"));
+        primaryStage.setWidth(xResolution);
+        primaryStage.setHeight(yResolution);
         primaryStage.setResizable(false);
         
-        sc = new SlideContent();
-        for (Slide currentSlide : slideList) {
-        	if (currentSlide.getTitle().equals("First Slide")) {
-	        	primaryStage.setScene(new Scene(sc.loadSlide(currentSlide.getTitle())));
-        	}
-        	System.out.println(currentSlide.getTitle());
-        }
+        root = new Group();
+        loadSlide(slideIndex);
         
+        /*GraphicHandler gh = new GraphicHandler(root, xResolution, yResolution);
+        GraphicElement graphic = gh.new GraphicElement("oval", 0.25f, 0.25f, 0.75f, 0.75f, true, 0, "#FFFF0000", "#00000000");*/
+        
+	    primaryStage.setScene(new Scene(root));
+              
+	}
+	
+	public void loadSlide(int id) {
+		
+		root.getChildren().clear();
+		currentSlide = slideList.get(id);
+		
+		//GraphicHandler gh = new GraphicHandler(root, xResolution, yResolution);
+		/*List<Graphic> graphicList = slideList.get(id).getGraphicList();
+		for (Graphic currentGraphic : graphicList) {
+			GraphicElement graphic = gh.new GraphicElement(currentGraphic.getType(),
+					currentGraphic.getXstart(),
+					currentGraphic.getYstart(),
+					currentGraphic.getXend(),
+					currentGraphic.getYend(),
+					currentGraphic.isSolid(),
+					currentGraphic.getDuration(),
+					currentGraphic.getGraphicColor(),
+					currentGraphic.getGraphicColor());
+		}*/
+		
+		
+		//ImageHandler ih = new ImageHandler();
+		/* something else here */
+		//slide.getChildren().add(img);
 	}
 	
 	@Override
