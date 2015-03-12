e86cb401c3b8cfa250595d8426c9e4d68ce362d3
aph513 <aph513@ELCLASS24.its.york.ac.uk>
2015-03-12 16:37:43 +0000

    Added buttons to solve merging error.
    Signed-off-by: aph513 <aph513@ELCLASS24.its.york.ac.uk>

---
+++ EyeView/src/Button/ButtonType.java
@@ -0,0 +1,32 @@
+package Button;
+
+public class ButtonType {
+	public String colourRGB;
+	public String borderColourRGB;
+	public String text;
+	public double xSize;
+	public double ySize;
+
+	/**
+	 * Button variable container
+	 * 
+	 * @param colourRGB
+	 *            Button fill colour (RR,GG,BB).
+	 * @param borderColourRGB
+	 *            Button border colour (RR,GG,BB).
+	 * @param text
+	 *            Button text.
+	 * @param xSize
+	 *            Button width (Pixels).
+	 * @param ySize
+	 *            Button height (Pixels).
+	 */
+	public ButtonType(String colourRGB, String borderColourRGB, String text,
+			double xSize, double ySize) {
+		this.colourRGB = colourRGB;
+		this.borderColourRGB = borderColourRGB;
+		this.text = text;
+		this.xSize = xSize;
+		this.ySize = ySize;
+	}
+}
