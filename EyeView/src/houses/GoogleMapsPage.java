7e06dd36610eef43a8b2df661a2bf2217ae1fecb
fahimrahman <fahimrahman17@gmail.com>
2015-06-01 18:36:01 +0100

    maps commented
    Signed-off-by: fahimrahman <fahimrahman17@gmail.com>

--- EyeView/src/houses/GoogleMapsPage.java
+++ EyeView/src/houses/GoogleMapsPage.java
@@ -3,7 +3,6 @@ package houses;
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
-
 import database.Database;
 import database.House;
 import javafx.geometry.Pos;
@@ -19,6 +18,15 @@ import language.Translator;
 import presenter.Window;
 import profile.SavedProperties;
 
+/**
+ * This class puts an embedded browser in the scene, which can render HTML
+ * content from local and remote URLs. This helps to load the JavaScript version
+ * of the Google Maps API, which displays the house location and route from the
+ * university.
+ * 
+ * @version 2.1
+ * @author Copyright (c) 2015 EyeHouse Ltd. All rights reserved.
+ */
 public class GoogleMapsPage extends Window {
 
 	MyBrowser myBrowser;
@@ -26,6 +34,9 @@ public class GoogleMapsPage extends Window {
 	private GridPane grid = new GridPane();
 	private Label topTitle;
 
+	/**
+	 * Constructor method
+	 */
 	public GoogleMapsPage() {
 
 		myBrowser = new MyBrowser();
@@ -43,55 +54,70 @@ public class GoogleMapsPage extends Window {
 		root.getChildren().add(pane);
 	}
 
+	/**
+	 * Sets up the Grid's horizontal and vertical spacing.
+	 */
 	public void setupGrid() {
 
 		grid.setVgap(10);
 		grid.setHgap(10);
 	}
 
-
+	/**
+	 * Sets up the the title of the page.
+	 */
 	public void setupTitle() {
 
-		topTitle = new Label(Translator.translateText(languageIndex, "Map and Route"));
+		topTitle = new Label(Translator.translateText(languageIndex,
+				"Map and Route"));
 		topTitle.setFont(new Font(32));
 		topTitle.setTextFill(Color.web("#162252"));
 		topTitle.setPrefWidth(550);
 		topTitle.setAlignment(Pos.CENTER);
 		topTitle.relocate(275, 80);
 		root.getChildren().add(topTitle);
-	}	
-	
-	public void updateLanguage() {
-		
-		topTitle.setText(Translator.translateText(languageIndex, "Map and Route"));
 	}
 
+	/**
+	 * Makes sure that the title is translated.
+	 */
+	public void updateLanguage() {
+
+		topTitle.setText(Translator.translateText(languageIndex,
+				"Map and Route"));
+	}
+
+	/**
+	 * Sets up the back button for the page.
+	 */
 	public void setupButtons() {
 
 		SavedProperties.setupPropertyBackButton();
 	}
 
+	/**
+	 * This class utilises WebEngine, which helps navigating links and loading
+	 * HTML content. And it contains WebView, which encapsulates a WebEngine
+	 * object and incorporates HTML content into an application's scene.
+	 */
 	class MyBrowser extends Region {
 
 		WebView webView;
 		WebEngine webEngine;
 
-		/*
-		 * public void getHouseLocation() { String house =
-		 * "36, Eastfield Crescent, YO10 5JB";
-		 * webEngine.executeScript("document.zoomIn()");
-		 * 
-		 * }
+		/**
+		 * Constructor method
 		 */
-
 		public MyBrowser() {
 
 			webView = new WebView();
 			webEngine = webView.getEngine();
-			
+
+			// Retrieval of house location from the database.
 			House destination = Database.getHouse(currentPropertyID);
-			changeDestination(destination.address,destination.postcode);
-			
+			changeDestination(destination.address, destination.postcode);
+
+			// Accessing the HTML file.
 			File file = new File("src/houses/PropertyMapData.html");
 			try {
 				URL urlGoogleMaps = file.toURI().toURL();
@@ -99,13 +125,24 @@ public class GoogleMapsPage extends Window {
 			} catch (MalformedURLException e) {
 				System.out.println("Property map data not found.");
 			}
-			
+
+			// Adding the browser to the grid.
 			getChildren().add(webView);
 			grid.add(webView, 1, 2);
 			getChildren().add(grid);
 
 		}
 
+		/**
+		 * Sends in a scanner into the HTML file, finds the line which set the
+		 * destination location and changes it to the house location retrived
+		 * from the database.
+		 * 
+		 * @param address
+		 *            The street address of the house from the database.
+		 * @param postcode
+		 *            The postcode of the house from the database.
+		 */
 		private void changeDestination(String address, String postcode) {
 
 			URL urlGoogleMaps = getClass().getResource("GoogleMaps.html");
@@ -115,12 +152,16 @@ public class GoogleMapsPage extends Window {
 				Scanner in = new Scanner(new InputStreamReader(
 						inputConnection.getInputStream()));
 
-				PrintWriter output = new PrintWriter(new OutputStreamWriter(
-					       new FileOutputStream("src/houses/PropertyMapData.html")));
+				PrintWriter output = new PrintWriter(
+						new OutputStreamWriter(new FileOutputStream(
+								"src/houses/PropertyMapData.html")));
+				
+				// Setting the destination line to the house location.
 				while (in.hasNext()) {
 					String inputLine = in.nextLine();
-					if(inputLine.contains("var destinationA")){
-						inputLine = "	var destinationA = '"+ address +", " + postcode + "';";
+					if (inputLine.contains("var destinationA")) {
+						inputLine = "	var destinationA = '" + address + ", "
+								+ postcode + "';";
 					}
 					output.println(inputLine);
 				}
