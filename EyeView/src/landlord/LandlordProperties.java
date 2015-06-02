6e287995b86ed11101455c804ba0d89610161084
aph513 <aph513@ELCLASS19.its.york.ac.uk>
2015-06-02 02:43:08 +0100

    Finished commenting.
    Signed-off-by: aph513 <aph513@ELCLASS19.its.york.ac.uk>

--- EyeView/src/landlord/LandlordProperties.java
+++ EyeView/src/landlord/LandlordProperties.java
@@ -31,7 +31,7 @@ import database.HouseImage;
 import database.User;
 
 /**
- * This class creates the landlord property page
+ * This class creates the landlord properties page
  * 
  * @version 1.8 01.06.15
  * @author EyeHouse
@@ -40,7 +40,7 @@ import database.User;
  */
 public class LandlordProperties extends Window {
 
-	/* Landlord Properties instance variables */
+	// Landlord Properties instance variables
 	GridPane grid = new GridPane();
 	ArrayList<House> properties = new ArrayList<House>();
 	ListView<HBox> propertyList = new ListView<HBox>();
@@ -55,69 +55,75 @@ public class LandlordProperties extends Window {
 	 */
 	public LandlordProperties(String pageUsername) {
 
-		/* Retrieve current user information from database */
+		// Retrieve current user information from database
 		viewedUser = Database.getUser(pageUsername);
 
-		/* Setup size and spacing of gridpane layout object */
+		// Setup size and spacing of gridpane layout object
 		setupGrid();
 
-		/* Add the title to the screen */
+		// Add the title to the screen
 		setupTitle();
 
-		/* Add controls to the screen, and handle the inputs */
+		// Add controls to the screen, and handle the inputs
 		setupLandlordButtons();
 
-		/* Populate list with owned properties */
+		// Populate list with owned properties
 		setupPropertyList();
 
-		/* Add back button to the screen */
+		// Add back button to the screen
 		SlideContent.setupBackButton();
 
-		/* Add layout object to the screen group */
+		// Add layout object to the screen group
 		root.getChildren().add(grid);
 	}
 
-	/* Setup grid layout object */
+	/**
+	 * Setup grid layout object 
+	 */
 	private void setupGrid() {
 
-		/* Setup column sizes */
+		// Setup column sizes
 		ColumnConstraints col1 = new ColumnConstraints();
 		ColumnConstraints col2 = new ColumnConstraints();
 		col1.setMinWidth(500);
 		col2.setMinWidth(100);
 		grid.getColumnConstraints().addAll(col1, col2);
 
-		/* Setup grid spacing */
+		// Setup grid spacing
 		grid.setVgap(30);
 		grid.setHgap(30);
 		grid.relocate(220, 130);
 	}
 
-	/* Setup title text */
+	/**
+	 * Setup title text
+	 */
 	private void setupTitle() {
 
-		/* Create label in chosen language */
+		// Create label in chosen language
 		Label labelTitle = new Label(Translator.translateText(languageIndex,
 				"Manage Properties"));
 
-		/* Configure style of text */
+		// Configure style of text
 		labelTitle.setTextFill(Color.web("#162252"));
 		labelTitle.setFont(new Font(35));
 		labelTitle.setPrefWidth(550);
 		labelTitle.setAlignment(Pos.CENTER);
 
-		/* Locate title and add to the group */
+		// Locate title and add to the group
 		labelTitle.relocate(275, 80);
 		root.getChildren().add(labelTitle);
 	}
 
-	/* Setup control buttons */
+	/** 
+	 * Setup control buttons
+	 */
 	private void setupLandlordButtons() {
 
-		/* Create box to contain buttons */
+		// Create box to contain buttons
 		VBox buttons = new VBox(30);
 
-		/* Create buttons in chosen language */
+		// Create buttons in chosen language
 		ButtonType button1 = new ButtonType("166,208,255", null,
 				Translator.translateText(languageIndex, "Edit"), 100, 30);
 		ButtonType button2 = new ButtonType("166,208,255", null,
@@ -132,22 +138,27 @@ public class LandlordProperties extends Window {
 		Button buttonNew = new SetupButton().CreateButton(button3);
 		Button buttonView = new SetupButton().CreateButton(button4);
 
-		/* Set cursor image on hover */
+		// Set cursor image on hover
 		buttonEdit.setCursor(Cursor.HAND);
 		buttonDelete.setCursor(Cursor.HAND);
 		buttonNew.setCursor(Cursor.HAND);
 		buttonView.setCursor(Cursor.HAND);
 
-		/* Set button handler for edit button */
+		// Set button handler for "Edit" button
 		buttonEdit.setOnAction(new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent event) {
 				
-				/* Check a property has been selected */
+				// Check a property has been selected
 				if (propertyList.getSelectionModel().getSelectedIndex() >= 0) {
+					
+					// Check which property is selected
 					int index = propertyList.getSelectionModel()
 							.getSelectedIndex();
+					
+					// Set the current property ID to the selected house
 					currentPropertyID = properties.get(index).hid;
 
+					// Open the edit property page
 					loadSlide(EDITPROPERTY);
 				}
 			}
@@ -157,25 +168,25 @@ public class LandlordProperties extends Window {
 		buttonDelete.setOnAction(new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent event) {
 				
-				/* Check a property has been selected */
+				// Check a property has been selected
 				if (propertyList.getSelectionModel().getSelectedIndex() >= 0) {
 					
-					/* Check which property is selected */
+					// Check which property is selected
 					int index = propertyList.getSelectionModel()
 							.getSelectedIndex();
 
-					/* Shuffle items after deletion up the list */
+					// Shuffle items after deletion up the list
 					for (int i = index; i < items.size() - 1; i++) {
 						items.set(i, items.get(i + 1));
 					}
 
-					/* Remove last item which is now a duplicate */
+					// Remove last item which is now a duplicate
 					items.remove(items.size() - 1);
 
-					/* Remove the selected house from the database */
+					// Remove the selected house from the database
 					Database.houseDelete(properties.get(index), viewedUser);
 					
-					/* Remove the selected house from local list of houses */
+					// Remove the selected house from local list of houses
 					properties.remove(index);
 
 				}
@@ -186,73 +197,81 @@ public class LandlordProperties extends Window {
 		buttonView.setOnAction(new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent event) {
 
-				/* Check a property has been selected */
+				// Check a property has been selected
 				if (propertyList.getSelectionModel().getSelectedIndex() >= 0) {
 					
-					/* Check which property is selected */
+					// Check which property is selected
 					int index = propertyList.getSelectionModel()
 							.getSelectedIndex();
+					
+					// Set the current property ID to the selected house
 					currentPropertyID = properties.get(index).hid;
+					
+					// Open the single property view page
 					loadSlide(HOUSE);
 				}
 			}
 		});
 
-		/* Set button handler for "New" button */
+		// Set button handler for "New" button
 		buttonNew.setOnAction(new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent event) {
 
-				/* Load an edit property slide in the "create" configuration */
+				// Load an edit property slide in the "create" configuration
 				currentPropertyID = 0;
 				loadSlide(EDITPROPERTY);
 			}
 		});
 
-		/* Add buttons to grid and align button group */
+		// Add buttons to grid and align button group
 		buttons.getChildren().addAll(buttonNew, buttonView, buttonEdit,
 				buttonDelete);
 		buttons.setAlignment(Pos.CENTER);
 		grid.add(buttons, 1, 1);
 	}
 
+	/**
+	 * Populate property list view with owned properties from the database.
+	 */
 	private void setupPropertyList() {
 
-		/* Set listview object height */
+		// Set listview object height
 		propertyList.setPrefHeight(550);
 
-		/* Retrieve owned properties from database */
+		// Retrieve owned properties from database
 		properties = Database.getLandlordProperties(viewedUser.uid);
 
-		/* Loop based on number of houses owned by landlord */
+		// Loop based on number of houses owned by landlord
 		for (int i = 0; i < properties.size(); i++) {
 
-			/* Setup property information containers */
+			// Setup property information containers
 			HBox listItem = new HBox(10);
 			VBox propertyInfo = new VBox(10);
 
-			/* Retrieve current property images */
+			// Retrieve current property images
 			ArrayList<HouseImage> houseImages = new ArrayList<HouseImage>();
 			houseImages = Database.getHouseImageSet(properties.get(i).hid);
 			
-			/* Setup thumbnail of first property image */
+			// Setup thumbnail of first property image
 			HouseImage input = houseImages.get(0);
 			ImageView thumbnail = new ImageView(new Image(input.imageIS));
 			
-			/* Resize thumbnail */
+			// Resize thumbnail
 			thumbnail.setFitHeight(100);
 			thumbnail.setFitWidth(100);
 
-			/* Get current property from list of landlord properties */
+			// Get current property from list of landlord properties
 			House house = properties.get(i);
 			
-			/* Create label with address of current property */
+			// Create label with address of current property
 			Label propertyAddress = new Label(house.address);
 			
-			/* Setup address label text wrapping and width */
+			//Setup address label text wrapping, width and make bold
 			propertyAddress.setWrapText(true);
 			propertyAddress.setMaxWidth(300);
+			propertyAddress.setFont(Font.font(null, FontWeight.BOLD, 20));
 			
-			/* Create label with price and number of bedrooms, translating field names */
+			// Create label with price and number of bedrooms, translating field names
 			Label propertyDetails = new Label(Translator.translateText(
 					languageIndex, "Bedrooms: ")
 					+ house.rooms
@@ -261,23 +280,20 @@ public class LandlordProperties extends Window {
 					+ " £"
 					+ house.price + " pppw");
 
-			/* Set the address label to bold */
-			propertyAddress.setFont(Font.font(null, FontWeight.BOLD, 20));
-
-			/* Add the address and house info labels to a VBox */
+			// Add the address and house info labels to a VBox
 			propertyInfo.getChildren().addAll(propertyAddress, propertyDetails);
 			
-			/* Add the thumbnail and property information to the current list item */
+			// Add the thumbnail and property information to the current list item
 			listItem.getChildren().addAll(thumbnail, propertyInfo);
 
-			/* Add the current list item to list */
+			// Add the current list item to list
 			items.add(listItem);
 		}
 		
-		/* Add the list to the listview object */
+		// Add the list to the listview object
 		propertyList.setItems(items);
 
-		/* Add the list view to the grid pane */
+		// Add the list view to the grid pane
 		grid.add(propertyList, 0, 1);
 	}
 
@@ -286,7 +302,7 @@ public class LandlordProperties extends Window {
 	 */
 	public void dispose() {
 
-		/* Clear all local lists and array lists to free memory space */
+		// Clear all local lists and array lists to free memory space
 		properties.clear();
 		properties.trimToSize();
 		properties = null;
--- EyeView/src/profile/SkypeCall.java
+++ EyeView/src/profile/SkypeCall.java
@@ -18,61 +18,117 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import language.Translator;
 
+/**
+ * This class implements skype calling in the form of a clickable skype logo.
+ * 
+ * @version 1.2 01.06.15
+ * @author EyeHouse
+ * 
+ *         Copyright 2015 EyeHouse
+ */
 public class SkypeCall extends Window {
 
+	/**
+	 * Create and return a skype call button based on the input Skype ID.
+	 * 
+	 * @param skypeID
+	 *            The Skype ID of the target contact
+	 * @return HBox A HBox containing the logo with hover text.
+	 */
 	public HBox addCallButton(String skypeID) {
 
+		// Create a HBox to contain the logo and hover label
 		final HBox skypeBox = new HBox(5);
-		
-		// Hidden label appears next to button when enabled and hovered over
-		final Label skypeLabel = new Label(Translator.translateText(languageIndex,
-				"Call on Skype"));
+
+		// Create the hover label and set to bold
+		final Label skypeLabel = new Label(Translator.translateText(
+				languageIndex, "Call on Skype"));
 		skypeLabel.setFont(Font.font(null, FontWeight.BOLD, 14));
-		
+
+		// Create an imageview of the skype lgo
 		ImageView skypeLogo = new ImageView(new Image(
 				"file:resources/images/Skype.png"));
+		
+		// Prevent warping of the logo
 		skypeLogo.setPreserveRatio(true);
+		
+		// Set the cursor to appear as a hand when hovering over the button
 		skypeLogo.setCursor(Cursor.HAND);
+		
+		// Hidden label appears next to button when the mouse enters the button
 		skypeLogo.setOnMouseEntered(new EventHandler<MouseEvent>() {
 			public void handle(MouseEvent arg0) {
 				skypeBox.getChildren().add(skypeLabel);
 			}
 		});
+		
+		// Hidden label disappears when mouse exits the button
 		skypeLogo.setOnMouseExited(new EventHandler<MouseEvent>() {
 			public void handle(MouseEvent arg0) {
 				skypeBox.getChildren().remove(skypeLabel);
 			}
 		});
+		
+		// Add the call handler to the imageview mouse click event
 		skypeLogo.setOnMouseClicked(new callHandler(skypeID));
 
+		// Left align the HBox
 		skypeBox.setAlignment(Pos.CENTER_LEFT);
+		
+		// Add the Skype logo to the HBox
 		skypeBox.getChildren().add(skypeLogo);
 
+		// Return the populated HBox
 		return skypeBox;
 	}
 
+	/**
+	 * This event handler handles skype calling when the skype logo is pressed.
+	 * 
+	 * @author EyeHouse
+	 * 
+	 */
 	private class callHandler implements EventHandler<MouseEvent> {
 
 		final String skypeID;
 
-		callHandler(String skypeID) {
+		/**
+		 * Mouse Event Constructor
+		 * 
+		 * @param skypeID
+		 * 			Skype ID to be called when logo is pressed.
+		 */
+		public callHandler(String skypeID) {
 			this.skypeID = skypeID;
 		}
 
+		/**
+		 * Mouse Event Handler
+		 */
 		@Override
 		public void handle(MouseEvent arg0) {
 			try {
+				
+				// Construct a URI containing the target skype contact
 				URI uri = new URI("skype:" + skypeID + "?call");
+				
+				// If the desktop is available
 				if (Desktop.isDesktopSupported()) {
 					try {
+						
+						// Execute the URI
 						Desktop.getDesktop().browse(uri);
 					} catch (IOException e) {
+						
+						// Open a popup if skype is not installed on the computer
 						createWarningPopup("Skype must be installed to utilise this feature.");
 						dialogStage.show();
 					}
 				}
 			} catch (URISyntaxException e) {
-				e.printStackTrace();
+				
+				// Catch URI syntax errors
+				System.out.println("Invalid Skype ID");
 			}
 		}
 
